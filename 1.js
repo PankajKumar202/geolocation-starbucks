864fdeafd6e609bbfc955b275463544a0a2b44f2
PankajKumar202 <parya209@gmail.com>
2022-01-20 19:55:55 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,82 @@
+
+
+function closeDiv(){
+    document.getElementById('coupon').style.visibility="hidden"
+}
+
+function test(){
+    document.getElementById('coupon').style.visibility="visible"
+}
+ 
+function mouseOverCoffee(){
+    document.getElementById('coffeeDropDown').style.visibility="visible"
+}
+
+function mouseOutCoffee(){
+    document.getElementById('coffeeDropDown').style.visibility="hidden"
+}
+
+function mouseOverMenu(){
+    document.getElementById('menuDropDown').style.visibility="visible"
+}
+
+function mouseOutMenu(){
+    document.getElementById('menuDropDown').style.visibility="hidden"
+}
+
+function mouseOverResponsibility(){
+    document.getElementById('responsibilityDropDown').style.visibility="visible"
+}
+
+function mouseOutResponsibility(){
+    document.getElementById('responsibilityDropDown').style.visibility="hidden"
+}
+
+function mouseOverCoffeeHouse(){
+    document.getElementById('coffeeHouseDropDown').style.visibility="visible"
+}
+
+function mouseOutCoffeeHouse(){
+    document.getElementById('coffeeHouseDropDown').style.visibility="hidden"
+}
+
+function mouseOverAboutUs(){
+    document.getElementById('aboutUsDropDown').style.visibility="visible"
+}
+
+function mouseOutAboutUs(){
+    document.getElementById('aboutUsDropDown').style.visibility="hidden"
+}
+// location and weather
+
+let Geolocation=()=>{
+  
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geo not support")
+    }
+
+}
+
+let showPosition=(data)=>{
+    let y = document.getElementById("output");
+    let z =  document.getElementById('icon')
+    let lon=data.coords.longitude;
+    let lat=data.coords.latitude;
+    console.log(data);
+    let url=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+   
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+        data.list.map((item)=>{
+            console.log(item.temp.day)
+            y.innerText=`${item.temp.day}°C`;
+            z.innerHTML=`<img class='card-img-top' src='https://openweathermap.org/img/w/${item.weather[0].icon}.png' alt='weather'/>`
+        })
+        
+    })
+}
+Geolocation();
